ea3d7f0f7bb08078115c91fa4fbe3d1da6843f3c
Perttu Auramo <perttu.auramo@gmail.com>
2015-03-28 17:42:40 +0200

    cljs node repl

---
+++ node-repl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+hash rlwrap 2>/dev/null || { echo >&2 "rlwrap required, install it!  Aborting."; exit 1; }
+hash node 2>/dev/null || { echo >&2 "node required, install it!  Aborting."; exit 1; }
+
+rlwrap lein trampoline run -m clojure.main node_repl.clj
