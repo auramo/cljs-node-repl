d77119c3482431c305d7ae4738d74325702b5877
Perttu Auramo <perttu.auramo@gmail.com>
2015-03-28 17:59:00 +0200

    add check for leiningen

--- node-repl.sh
+++ node-repl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+hash rlwrap 2>/dev/null || { echo >&2 "lein (leiningen) required, install it!  Aborting."; exit 1; }
 hash rlwrap 2>/dev/null || { echo >&2 "rlwrap required, install it!  Aborting."; exit 1; }
 hash node 2>/dev/null || { echo >&2 "node required, install it!  Aborting."; exit 1; }
 
